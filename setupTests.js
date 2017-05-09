512030478d9db625ea8240e5239d0ad6ba59d478
TalH <tal@soluto.com>
2017-05-09 18:03:00 +0300

    no mocking animated

--- setupTests.js
+++ setupTests.js
@@ -1,3 +1,3 @@
 require('./ignoreUnknownPropsError')
-jest.mock('Animated',()=> require('./node_modules/react-native-mock-render/build/react-native.js').Animated)
+//jest.mock('Animated',()=> require('./node_modules/react-native-mock-render/build/react-native.js').Animated)
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 60 * 60 * 1000;
